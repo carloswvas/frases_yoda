43e4b55ac50b4535992b0735720e0fc92e718c67
Carlos Wilton <carlosvasconcelos.dev@gmail.com>
2022-05-05 20:46:34 -0300

    Funcionalidade Pronta

--- lib/src/home_screen.dart
+++ lib/src/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,6 +10,23 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final _frases = [
+    'Guerras Não Fazem Ninguém Maior',
+    'Um Jedi usa a força para conhecimento e defesa, nunca para ataque',
+    'Raiva, medo, agressão. Ao lado sombrio elas pertencem',
+    'Você deve desaprender o que aprendeu',
+    'Sempre em movimento é o Futuro',
+    'Fazer ou não fazer. Tentativa não há'
+  ];
+  var _texto = 'Clique no botão para gerar uma frase.';
+
+  _gerarFrase() {
+    var numeroRandom = Random().nextInt(_frases.length);
+    setState(() {
+      _texto = _frases[numeroRandom];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,26 +39,35 @@ class _HomeScreenState extends State<HomeScreen> {
         padding: const EdgeInsets.symmetric(horizontal: 8),
         width: double.infinity,
         height: double.infinity,
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Image(
-                image: AssetImage('../images/yoda.png'),
-                height: 300.0,
-              ),
-              const Text(
-                'Clique a baixo para gerar uma frase',
-                style: TextStyle(fontSize: 22.0),
-              ),
-              ElevatedButton(
-                  onPressed: () => {},
-                  child: const Text(
-                    'Nova Frase',
-                    style: TextStyle(fontSize: 18.0),
+        child: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Image(
+                  image: AssetImage('../images/yoda.png'),
+                  height: 300.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    _texto,
+                    style: const TextStyle(
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 5,
+                    ),
                   ),
-                  style: ElevatedButton.styleFrom(primary: Colors.green)),
-            ]),
+                ),
+                ElevatedButton(
+                    onPressed: () => _gerarFrase(),
+                    child: const Text(
+                      'Nova Frase',
+                      style: TextStyle(fontSize: 18.0),
+                    ),
+                    style: ElevatedButton.styleFrom(primary: Colors.green)),
+              ]),
+        ),
       ),
     );
   }
